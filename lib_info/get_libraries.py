9bc2549ab168e2ce5dca94f0f79e40640f3a1671
Marcos Iglesias <miglesias@eventbrite.com>
2017-05-13 22:57:37 -0700

    Updating

--- lib_info/get_libraries.py
+++ lib_info/get_libraries.py
@@ -2,14 +2,20 @@ import json
 import requests
 
 URLs = [
-    'd3fc/d3fc',
-    'vega/vega',
-    'novus/nvd3',
-    'robinfhu/forest-d3',
-    'palantir/plottable',
-    'plotly/plotly.js',
-    'misoproject/d3.chart',
-    'mozilla/metrics-graphics',
+    # 'd3fc/d3fc',
+    # 'vega/vega',
+    # 'novus/nvd3',
+    # 'robinfhu/forest-d3',
+    # 'palantir/plottable',
+    # 'plotly/plotly.js',
+    # 'misoproject/d3.chart',
+    # 'mozilla/metrics-graphics',
+    "alexandersimoes/d3plus",
+    "c3js/c3",
+    "heavysixer/d4",
+    "epochjs/epoch",
+    "pykih/PykCharts.js",
+    "shutterstock/rickshaw",
 ]
 
 results = {}
@@ -18,5 +24,5 @@ for url in URLs:
     r = requests.get('https://api.github.com/repos/' + url, headers={'Accept': 'application/vnd.github.preview'})
     results[url] = json.loads(r.text)
 
-with open('d3-libraries.json', 'w') as outfile:
+with open('d3-libraries-bis.json', 'w') as outfile:
     json.dump(results, outfile)
