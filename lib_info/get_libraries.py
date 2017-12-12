fee3853d8e118d0595de5d4eef3c5cf216533c49
Marcos Iglesias <miglesias@eventbrite.com>
2017-12-11 17:49:34 -0800

    Updating libraries script and json values

--- lib_info/get_libraries.py
+++ lib_info/get_libraries.py
@@ -37,37 +37,50 @@ d3Libraries = [
 reactD3Libraries = [
     'eventbrite/britecharts-react',
     'recharts/recharts',
-    # 'FormidableLabs/victory',
-    # 'uber/react-vis',
-    # 'hshoff/vx',
-    # 'artyomtrityak/d3-explorer',
-    # 'react-d3',
-    # 'yang-wei/rd3',
-    # 'codesuki/react-d3-components',
-    # 'react-d3-library/react-d3-library',
-    # 'bgrsquared/d3-react-squared',
-    # 'plouc/nivo',
-    # 'emeeks/semiotic',
+    'FormidableLabs/victory',
+    'uber/react-vis',
+    'hshoff/vx',
+    'artyomtrityak/d3-explorer',
+    'react-d3',
+    'yang-wei/rd3',
+    'codesuki/react-d3-components',
+    'react-d3-library/react-d3-library',
+    'bgrsquared/d3-react-squared',
+    'plouc/nivo',
+    'emeeks/semiotic',
 ]
 
 
-# resultsD3libraries = {}
-
-# for url in d3Libraries:
-#     r = requests.get('https://api.github.com/repos/' + url, headers={'Accept': 'application/vnd.github.preview'})
-#     resultsD3libraries[url] = json.loads(r.text)
-
-# with open('d3-lib.json', 'w') as outfile:
-#     json.dump(resultsD3libraries, outfile)
-
+resultsD3libraries = {}
+fD3 = {}
 resultsReactD3libraries = {}
+fRD3 = {}
+
+for url in d3Libraries:
+    r = requests.get('https://api.github.com/repos/' + url, headers={'Accept': 'application/vnd.github.preview'})
+    resultsD3libraries[url] = json.loads(r.text)
 
 for url in reactD3Libraries:
     r = requests.get('https://api.github.com/repos/' + url, headers={'Accept': 'application/vnd.github.preview'})
-    rawResults = json.loads(r.text)
-    resultsReactD3libraries[url] = map(reactD3Libraries.get, keys)
+    resultsReactD3libraries[url] = json.loads(r.text)
 
-# r = requests.get('https://api.github.com/repos/eventbrite/britecharts-react', headers={'Accept': 'application/vnd.github.preview'})
+
+for url, lib_info in resultsD3libraries.iteritems():
+    fD3[lib_info.get('name')] = {
+        key: value
+        for key, value in lib_info.iteritems()
+        if key in keys
+    }
+
+for url, lib_info in resultsReactD3libraries.iteritems():
+    fRD3[lib_info.get('name')] = {
+        key: value
+        for key, value in lib_info.iteritems()
+        if key in keys
+    }
+
+with open('d3-lib.json', 'w') as outfile:
+    json.dump(fD3, outfile)
 
 with open('react-d3-lib.json', 'w') as outfile:
-    json.dump(resultsReactD3libraries, outfile)
+    json.dump(fRD3, outfile)
